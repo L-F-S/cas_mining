bb3b4acd7d28266708f8299324f0e5e789557fb5
L-F-S <lorenzo.fsignorini@outlook.it>
2020-04-21 18:19:33 +0200

    cleaned comments

--- 9casminer_v2/get_ID_info.py
+++ 9casminer_v2/get_ID_info.py
@@ -12,10 +12,7 @@ python get_ID_info.py -h
 
 
 """
-#TODO onc eI automatize tracrfinding, we will be able to make this into a
-#uuseful tool
 import os
-import subprocess
 import sys
 import pandas as pd
 import argparse
@@ -25,7 +22,6 @@ from Bio.Align.Applications import ClustalwCommandline
 
 import locus
 sys.path.insert(0, '/home/lorenzo.signorini/cas_mining/utils/')
-import filename_discrepancies
 import originalpath
 
 def vprint(string):
@@ -35,18 +31,25 @@ def vprint(string):
 
 def get_ID_info(seqid, feature,v, saveout, outdir,tracrRNA, tracrstrand, crarraystrand,repeat,wdir):
     os.chdir(wdir)
-#    outdir+="test/"
-    info_text=""
+    # intialize Cas locus object, from cas dataset
     cas_dataset=pd.read_csv(wdir+"5caslocitable/known_"+feature+"_variants_table.csv", index_col=0)
     caslocus=locus.locus(seqid,feature)
     caslocus.fill_from_dataset(cas_dataset)
+    # get CRISPRarray and positions
+    tmp=locus.CRISPRarray(feature=feature, contigname=caslocus.contigname,genomename=caslocus.genomename,datasetname=caslocus.datasetname)
+    tmp.get_CRISPR_array()
+    if crarraystrand=="-":
+        tmp.rev_comp()
+    caslocus.CRISPRarray=tmp
+    caslocus.fetch_positions(cas_dataset)
+    # Print main info
+    info_text=""
     temp_print="-"*80+"\n"+" "*int((80- len("Sequence ID"))/2)+" Sequence ID"+\
           " "*int((80- len("Sequence ID"))/2)+"\n"+\
           " "*int((80- len("Sequence ID"))/2)+ seqid+\
           " "*int((80- len("Sequence ID"))/2)+"\n"+"-"*80+"\n"
     info_text+=temp_print+"\n"
     print(temp_print)
-    sequence_length=len(caslocus.seq)
     temp_print="-> Contig Name:\t"+caslocus.contigname
     info_text+=temp_print+"\n"
     print(temp_print)
@@ -68,19 +71,11 @@ def get_ID_info(seqid, feature,v, saveout, outdir,tracrRNA, tracrstrand, crarray
     temp_print="-> Estimated taxonomy:\t"+caslocus.estimated_taxonomy
     info_text+=temp_print+"\n"
     print(temp_print)
-    temp_print="-> " +feature+" sequence length:\t"+str(sequence_length)
+    temp_print="-> " +feature+" sequence length:\t"+str(len(caslocus.seq))
     info_text+=temp_print+"\n"
     print(temp_print)
-
     if v:
-        print("\n-> Sequence:\n\n", caslocus.seq)
-    # get array
-    tmp=locus.CRISPRarray(feature=feature, contigname=caslocus.contigname,genomename=caslocus.genomename,datasetname=caslocus.datasetname)
-    tmp.get_CRISPR_array()
-    if crarraystrand==-1:
-        tmp.rev_comp()
-    caslocus.CRISPRarray=tmp
-    caslocus.fetch_positions(cas_dataset)
+       vprint("\n-> Sequence:\n\n", caslocus.seq)
     temp_print="-> Positions inside contig:\t"+str(caslocus.positions)
     print(temp_print)
     info_text+=temp_print+"\n"
@@ -94,11 +89,11 @@ def get_ID_info(seqid, feature,v, saveout, outdir,tracrRNA, tracrstrand, crarray
         print(temp_print)
         temp_print="-> CRISPRarray strand:\t"+str(args.c)
         info_text+=temp_print+"\n"
-
     CRISPRheader="REPEAT"+" "*(len(caslocus.CRISPRarray.repeats[0])-len("REPEAT"))+" SPACER"+" "*(len(caslocus.CRISPRarray.spacers)-len("SPACERS"))+"\n"
     CRISPRstrandstring="CRISPR array on FORWARD strand"
-    if crarraystrand==-1:
+    if crarraystrand=="-":
         CRISPRstrandstring="CRISPR array on REVERSE strand."
+        # verbose output: print CRISPR ARRAY
     vprint("CRISPR array sequence for "+ seqid+" "+feature+".")
     vprint(CRISPRstrandstring)
     vprint(CRISPRheader)
@@ -114,6 +109,7 @@ def get_ID_info(seqid, feature,v, saveout, outdir,tracrRNA, tracrstrand, crarray
             vprint(line)
             CRISPRarraysequence+=line+"\n"
             break
+        # verbose output: print tracrRNA sequences
     if args.t:
         vprint("\n----------------------------------------------------------------------")
         vprint("         tracrRNA sequence for "+ feature +" protein id "+ seqid)
@@ -122,7 +118,7 @@ def get_ID_info(seqid, feature,v, saveout, outdir,tracrRNA, tracrstrand, crarray
     if repeat:
         vprint("Matching repeat:\n"+repeat)
 
-
+        # save output
     if saveout:
         print("\n----------------------------------------------------------------------")
         print("Saving output in: \n"+outdir)
@@ -143,7 +139,6 @@ def get_ID_info(seqid, feature,v, saveout, outdir,tracrRNA, tracrstrand, crarray
             f.write(">tracrRNA_"+seqid+"\n"+tracrRNA)
             f.close()
         print("Writing  CRISPRarray to CRISPR_"+seqid+".ffn")
-#        if crarraystrand==-1:
         f=open(outdir+seqid+"/CRISPR_"+seqid+".ffn", "w")
         f.close()
         f=open(outdir+seqid+"/CRISPR_"+seqid+".ffn", "a")
@@ -153,7 +148,7 @@ def get_ID_info(seqid, feature,v, saveout, outdir,tracrRNA, tracrstrand, crarray
         f.write(CRISPRarraysequence+"\n\n")
         if repeat:
             f.write("Matching repeat:\t"+repeat)
-#            os.popen("cp "+tmp.path+" "+outdir+seqid+"/CRISPR_"+seqid+".ffn")
+        f.close()
 
         print("Saving original genome")
         full_genome_full_path, annotation_folder=originalpath.print_path(caslocus.genomename)
@@ -166,7 +161,6 @@ def get_ID_info(seqid, feature,v, saveout, outdir,tracrRNA, tracrstrand, crarray
                 print(record.id)
                 print("++++++++++++++++++++++++++\n",record.seq)
                 record.description=feature+" len="+str(len(record.seq))+" genome="+caslocus.genomename+" SGB="+str(caslocus.SGB)+" contig="+caslocus.contigname
-                Cas9_fasta_header = ">"+seqid+" "+record.description
                 SeqIO.write(record, outdir+seqid+"/"+feature+"_"+seqid+".ffn","fasta")
                 break
 
@@ -196,8 +190,8 @@ if __name__=="__main__":
     main_descr="Print information about a given effector Cas Sequence ID."
     plus="+"*len(main_descr)
     parser=argparse.ArgumentParser(description="+"*5+"\t\t"+main_descr+"\t\t"+"+"*5 )
-    parser.add_argument("-v", action="store_true", help="verbose output")
     parser.add_argument("ID", type=str, help="sequence ID")
+    parser.add_argument("-v", action="store_true", help="verbose output")
     parser.add_argument("-f", type=str, help="effector Cas name (default= Cas9)", default="Cas9")
     parser.add_argument("-o", type=str, help="output directory, default =/shares/CIBIO-Storage/CM/news/users/lorenzo.signorini/9output/<feature>/", default="/shares/CIBIO-Storage/CM/news/users/lorenzo.signorini/9output/")
     parser.add_argument("-w", type=str, help="working directory, default =/shares/CIBIO-Storage/CM/news/users/lorenzo.signorini/", default="/shares/CIBIO-Storage/CM/news/users/lorenzo.signorini/")
@@ -205,7 +199,7 @@ if __name__=="__main__":
     parser.add_argument("-t", type=str, help="tracRNA sequence")
     parser.add_argument("-s", type=str, help="tracrRNA strand. Possible values= +, -")
     parser.add_argument("-c", type=int, help="actual CRISPRarray strand (as discovered via previous tracrRNA analysis)\
-                        . Input \'+1\' if CRISPRarray strand is the same annotated from minced algorithm (forward strand), \'-1\' if reverse strand.")
+                        . Input \'+\' if CRISPRarray strand is the same annotated from minced algorithm (forward strand), \'-\' if reverse strand.")
     parser.add_argument("-r", type=str, help="repeat sequence matching tracrRNA")
     args=parser.parse_args()
     seqid =args.ID
@@ -219,6 +213,5 @@ if __name__=="__main__":
 
     if not os.path.exists(outdir):
             os.makedirs(outdir)
-
 
     get_ID_info(seqid, feature,args.v,args.d,outdir,tracrRNA, tracrstrand, crarraystrand,repeat,wdir)
